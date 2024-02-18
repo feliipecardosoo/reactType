943dfeb2b1aad99ebc2d45a1ca7d477f289ef7e7
feliipecardosoo <fglibino@gmail.com>
2024-02-18 18:26:49 -0300

    a

--- tarefas/src/App.tsx
+++ tarefas/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Formulario from './components/formulario';
+import Lista from './components/lista';
 
 
 function App() {
   return (
     <div className="App">
       <Formulario/> 
+      <Lista/> 
     </div>
   );
 }
---
+++ tarefas/src/components/lista/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Lista() {
+    return(
+        <aside>
+            <h2>
+                Estudos do dia
+            </h2>
+            <ul>
+                <li>
+                    <h3>
+                        React
+                    </h3>
+                    <span>
+                        02:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        Node
+                    </h3>
+                    <span>
+                        01:15:00
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista
