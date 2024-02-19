84d3878db1d8e6330aaf1aff79a451befd7c354c
feliipecardosoo <fglibino@gmail.com>
2024-02-19 20:01:24 -0300

    a

--- tarefas/src/components/lista/index.tsx
+++ tarefas/src/components/lista/index.tsx
@@ -1,28 +1,31 @@
 import React from "react";
 
 function Lista() {
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    },
+    {
+        tarefa: 'Node',
+        tempo: '00:30:00'
+    },
+    {
+        tarefa: 'Namorar a Leticia',
+        tempo: 'A vida toda'
+    }
+]
     return(
         <aside>
             <h2>
                 Estudos do dia
             </h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        Node
-                    </h3>
-                    <span>
-                        01:15:00
-                    </span>
-                </li>
+                {tarefas.map((item, index) => (
+                    <li>
+                        <h3>{item.tarefa}</h3>
+                        <span>{item.tempo}</span>
+                    </li>
+                ))}
             </ul>
         </aside>
     )
