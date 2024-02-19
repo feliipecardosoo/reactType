45b58e43b976f37ac6e94516fc984944a34dfa77
feliipecardosoo <fglibino@gmail.com>
2024-02-19 20:18:14 -0300

    a

--- tarefas/src/components/lista/index.tsx
+++ tarefas/src/components/lista/index.tsx
@@ -21,7 +21,7 @@ function Lista() {
             </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li>
+                    <li key={index}>
                         <h3>{item.tarefa}</h3>
                         <span>{item.tempo}</span>
                     </li>
