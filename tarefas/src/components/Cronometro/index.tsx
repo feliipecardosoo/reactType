87714822b995f15085fba4fbcbfca744b02cc5b6
feliipecardosoo <fglibino@gmail.com>
2024-02-20 19:45:29 -0300

    a

--- tarefas/src/components/Cronometro/index.tsx
+++ tarefas/src/components/Cronometro/index.tsx
@@ -1,11 +1,12 @@
 import Botao from "../botao"
 import Relogio from "./relogio"
+import style from './Cronometro.module.scss'
 
 export default function Cronometro() {
     return(
-        <div>
-            <p>Escolha um card e inicie o cronometro</p>
-            <div>
+        <div className={style.cronometro}>
+            <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
+            <div className={style.relogioWrapper}>
                 <Relogio/>
             </div>
             <Botao
